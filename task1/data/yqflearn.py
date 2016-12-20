e7e4f080e3393e4852824ef77c29f3a9535df16f
yqf3139 <yqf3139@gmail.com>
2016-12-20 09:56:54 +0800

    add report

--- task1/data/yqflearn.py
+++ task1/data/yqflearn.py
@@ -9,17 +9,23 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 import operator
 
-trash = {}
-
 class KNN(BaseEstimator, ClassifierMixin):
 
     def __init__(self, dis_func='euclidean', scale=True, K=1):
+        """
+        dis_func 距离函数，可取DistanceMetric中可用的距离函数名称
+        scale 是否对输入的特征向量进行均一化处理
+        K 使用多少个近邻
+        """
         self.dis_func = DistanceMetric.get_metric(dis_func)
         self.scale = scale
         self.K = K
         self.encoder = LabelEncoder()
 
     def select_class(self, neighbours):
+        """
+        选取近邻中类别最多的一类作为该样本的类别
+        """
         votes = [0] * len(self.classes_)
         classes = self.encoder.transform(self.y_train[neighbours])
         for id in classes:
@@ -27,7 +33,6 @@ class KNN(BaseEstimator, ClassifierMixin):
         return self.encoder.inverse_transform(np.argmax(votes))
 
     def fit(self, X, y):
-
         X, y = check_X_y(X, y)
         self.classes_ = unique_labels(y)
         self.encoder.fit(y)
@@ -43,7 +48,6 @@ class KNN(BaseEstimator, ClassifierMixin):
         return self
 
     def predict(self, X):
-
         check_is_fitted(self, ['X_train', 'y_train', 'scale', 'K'])
 
         X = check_array(X)
@@ -53,6 +57,8 @@ class KNN(BaseEstimator, ClassifierMixin):
 
         closest = None
 
+        # 为了节省内存和加快运算速度
+        # 每1000个样本批处理
         STEP = 1000
         k_enabled = self.K != 1 and self.K < len(X)
         for start in range(0, len(X), STEP):
@@ -88,7 +94,6 @@ class NativeBayes(BaseEstimator, ClassifierMixin):
         self.encoder = LabelEncoder()
 
     def fit(self, X, y):
-
         X, y = check_X_y(X, y)
         self.classes_ = unique_labels(y)
 
@@ -103,6 +108,7 @@ class NativeBayes(BaseEstimator, ClassifierMixin):
         dimension_data = [None] * len(self.classes_)
         self.dimension_data = dimension_data
 
+        # 计算先验概率等统计信息
         total_instances_num = len(y)
         dimension_num = X.shape[1]
         for class_name, class_id in zip(self.classes_, self.encoder.transform(self.classes_)):
@@ -125,6 +131,8 @@ class NativeBayes(BaseEstimator, ClassifierMixin):
         X = check_array(X)
         pred = np.zeros(len(X))
 
+        # 利用先验概率和统计信息
+        # 计算每个样本属于每一类的概率，选取概率最大的类标
         for ins_id, instance in enumerate(X):
             post_probabilities = [0.0] * len(self.classes_)
             for idx, class_data in enumerate(self.dimension_data):
